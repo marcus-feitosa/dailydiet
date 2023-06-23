491cb6fd00c81a80118cef9d7565f89ffd0a2518
marcus-feitosa <marcusfeitosa02@gmail.com>
2023-06-23 16:57:09 -0300

    Aplicando tema da aplicação e carregando fontes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
-import {Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
 
+import { Home } from './src/screens/Home';
 
+import { ActivityIndicator } from 'react-native';
+import theme from './src/theme';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts ({NunitoSans_400Regular, NunitoSans_700Bold});
+
+
   return (
-    <View>
-      <Text>Olá Mundo</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+       {fontsLoaded ? <Home /> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/index.ts
@@ -0,0 +1,34 @@
+export default{
+    COLORS: {
+
+        RED_500: '#BF3B44',
+        RED_300: '#F3BABD',
+        RED_100: '#F4E6E7',
+
+        GREEN_500 : '#639339',
+        GREEN_300: '#CBE4B4',
+        GREEN_100: '#E5F0DB',
+
+        GRAY_900 : '#1B1D1E',
+        GRAY_800: '#333638',
+        GRAY_700: '#5C6265',
+        GRAY_500: '#B9BBBC',
+        GRAY_400: '#DDDEDF',
+        GRAY_200: '#EFF0F0',
+        GRAY_100: '#FAFAFA',
+
+        WHITE: '#FFFFFF',
+
+    },
+    FONT_FAMILY : {
+        REGULAR: 'NunitoSans_400Regular',
+        BOLD: 'NunitoSans_700Bold'
+    },
+    FONT_SIZE: {
+        SM: 14,
+        MD: 16,
+        LG: 18,
+        XL: 24,
+        XXL: 32
+    }
+}
